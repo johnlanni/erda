eabb337433ca3c3542833122ec4e0655ddbc79c0
johnlanni <ztywto@qq.com>
2021-11-26 13:36:00 +0800

    update comment

--- modules/hepa/services/endpoint_api/interface.go
+++ modules/hepa/services/endpoint_api/interface.go
@@ -37,23 +37,43 @@ type PackageApiInfo struct {
 
 type GatewayOpenapiService interface {
 	Clone(context.Context) GatewayOpenapiService
+	// 清理所有流量入口内和对应 runtime service 关联的 api
 	ClearRuntimeRoute(id string) error
+	// 创建或更新 erda.yml 中指定的 endpoint 和 endpoint 的 api
 	SetRuntimeEndpoint(runtime_service.RuntimeEndpointInfo) error
+	// 创建或更新 APIM 里访问管理条目对应的根路径转发
 	TouchPackageRootApi(packageId string, reqDto *dto.OpenapiDto) (bool, error)
+	// 在 runtimeservice 的关联发生变化时（服务删除了、不依赖网关了）尝试清理对应的流量入口
 	TryClearRuntimePackage(*orm.GatewayRuntimeService, *service.SessionHelper, ...bool) error
+	// 创建或更新和 runtimeservice 关联的流量入口的元信息
 	TouchRuntimePackageMeta(*orm.GatewayRuntimeService, *service.SessionHelper) (string, bool, error)
+	// 更新和 runtimeservice 关联的流量入口下的所有 API 相关的 ingress
 	RefreshRuntimePackage(string, *orm.GatewayRuntimeService, *service.SessionHelper) error
+	// 为了 api policy 中适配统一域名入口，创建对应的 zone
 	CreateUnityPackageZone(string, *service.SessionHelper) (*orm.GatewayZone, error)
+	// 用于在创建项目租户时，创建对应的统一域名入口
 	CreateTenantPackage(string, *service.SessionHelper) error
+
+	// 创建流量入口
 	CreatePackage(*dto.DiceArgsDto, *dto.PackageDto) (*dto.PackageInfoDto, string, error)
+	// 获取流量入口列表
 	GetPackages(*dto.GetPackagesDto) (common.NewPageQuery, error)
+	// 获取流量入口详情
 	GetPackage(string) (*dto.PackageInfoDto, error)
+	// 获取租户下流量入口的名称
 	GetPackagesName(*dto.GetPackagesDto) ([]dto.PackageInfoDto, error)
+	// 编辑流量入口
 	UpdatePackage(string, string, *dto.PackageDto) (*dto.PackageInfoDto, error)
+	// 删除流量入口
 	DeletePackage(string) (bool, error)
+	// 创建流量入口下的 API
 	CreatePackageApi(string, *dto.OpenapiDto) (string, bool, error)
+	// 获取流量入口下的 API 列表
 	GetPackageApis(string, *dto.GetOpenapiDto) (common.NewPageQuery, error)
+	// 更新流量入口内的 API
 	UpdatePackageApi(string, string, *dto.OpenapiDto) (*dto.OpenapiInfoDto, bool, error)
+	// 删除流量入口内的 API
 	DeletePackageApi(string, string) (bool, error)
+	// 创建或修改流量入口 API 对应的 zone，同时创建对应的 ingress
 	TouchPackageApiZone(info PackageApiInfo, session ...*service.SessionHelper) (string, error)
 }
--- modules/hepa/services/micro_api/interface.go
+++ modules/hepa/services/micro_api/interface.go
@@ -28,15 +28,24 @@ var Service GatewayApiService
 
 type GatewayApiService interface {
 	Clone(context.Context) GatewayApiService
+	// 对应微服务 API 管理中，获取指定 runtimeservice 的 API 列表
 	GetRuntimeApis(runtimeServiceId string, registerType ...string) ([]dto.ApiDto, error)
+	// 在 kong 中创建 runtimeservice API 的内部接口
 	CreateRuntimeApi(dto *dto.ApiDto, session ...*service.SessionHelper) (string, vars.StandardErrorCode, error)
+	// 对接 kong 的 API 创建
 	CreateApi(*dto.ApiReqDto) (string, error)
+	// legacy openapi 接口，拿所有注册到 kong 里的 runtimeservice 以及 sdk 自动注册的 API
 	GetApiInfos(*dto.GetApisDto) (*common.PageQuery, error)
+	// 对接 kong 的 API 删除
 	DeleteApi(string) error
+	// 对接 kong 的 API 更新
 	UpdateApi(string, *dto.ApiReqDto) (*dto.ApiInfoDto, error)
+	// 创建 upstream api
 	CreateUpstreamBindApi(*orm.GatewayConsumer, string, string, string, *orm.GatewayUpstreamApi, string) (string, error)
 	UpdateUpstreamBindApi(*orm.GatewayConsumer, string, string, *orm.GatewayUpstreamApi, string) error
 	DeleteUpstreamBindApi(*orm.GatewayUpstreamApi) error
+	// 在 kong 中创建 runtimeservice 的 API
 	TouchRuntimeApi(*orm.GatewayRuntimeService, *service.SessionHelper, bool) error
+	// 对应清理 runtimeservice 的 API
 	ClearRuntimeApi(*orm.GatewayRuntimeService) error
 }
